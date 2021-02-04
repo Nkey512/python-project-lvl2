2cf62bd224b92afaace3a7ef7083a055032f6f0c
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-04 21:00:04 +0300

    Add help to gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,11 @@
 #!usr/bin/env python
 def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    print(args)
     print('Hello World!')
 
 
