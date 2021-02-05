6cda1ddf07774e475c23d795ef51d9c61a86fbce
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-05 20:57:42 +0300

    Various initial staff

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 install:
 	poetry install
-brain-games:
-	poetry run brain-games
+gendiffer:
+	poetry run gendiff
 build:
 	poetry build
 publish:
@@ -9,4 +9,4 @@ publish:
 package-install:
 	python3 -m pip install --user dist/*.whl
 lint:
-	poetry run flake8 brain_games
+	poetry run flake8 gendiff
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,9 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        help='set format of output',
+                        metavar='FORMAT')
     args = parser.parse_args()
     print(args)
     print('Hello World!')
