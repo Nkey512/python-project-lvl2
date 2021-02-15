be6751691634b8e919a5af6c54e53ed8fdb5a2d8
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-15 17:21:41 +0300

    Create tests for .yml format

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,9 +4,7 @@
 
 from gendiff.scripts.gendiff import generate_diff
 
-
-def test_flat_json():
-    string = '''{
+string = '''{
   - follow: False
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -15,5 +13,13 @@ def test_flat_json():
   + verbose: True
 }
 '''
+
+
+def test_flat_json():
     assert generate_diff('tests/fixtures/file1.json',
                          'tests/fixtures/file2.json') == string
+
+
+def test_flat_yaml():
+    assert generate_diff('tests/fixtures/file1.yml',
+                         'tests/fixtures/file2.yml') == string
