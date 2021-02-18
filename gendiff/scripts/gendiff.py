c36ba2eb72606abbded64c767933fffe490d74e4
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-18 18:25:21 +0300

    Another try

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,27 +2,17 @@
 from gendiff.parsing import parse_file
 
 
-def generate_diff(file1, file2):
-    keys1 = set(list(file1))
-    keys2 = set(list(file2))
-    all_keys = tuple(sorted({*keys1, *keys2}))
-    only_in_keys1 = keys1.difference(keys2)
-    only_in_keys2 = keys2.difference(keys1)
-    output = []
-    for key in all_keys:
-        if key in only_in_keys1:
-            output.append('  - {}: {}'.format(key, file1[key]))
-        elif key in only_in_keys2:
-            output.append('  + {}: {}'.format(key, file2[key]))
-        else:
-            value1 = file1[key]
-            value2 = file2[key]
-            if value1 == value2:
-                output.append('    {}: {}'.format(key, value1))
-            else:
-                output.append('  - {}: {}'.format(key, value1))
-                output.append('  + {}: {}'.format(key, value2))
-    return '{\n' + '\n'.join(output) + '\n}\n'
+def diff(doc1, doc2):
+    return
+
+
+def stylish(tree, tab=2):
+    return
+
+
+def generate_diff(file1, file2, formatter=stylish):
+    inner_d = diff(file1, file2)
+    return formatter(inner_d)
 
 
 def main():
