4f865a2cb158cc0586774c86e598eaab039d43f8
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-19 21:19:39 +0300

    Relocate cli code to separate module

---
+++ gendiff/cli.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def cli_argparser():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        help='set format of output',
+                        metavar='FORMAT')
+    return parser
