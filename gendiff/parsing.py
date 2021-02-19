9c1f2fc87a676f8edc597df44f643745833f34ca
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-19 20:38:43 +0300

    Improve parsing

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -1,10 +1,21 @@
+import os
 import json
 import yaml
 
 
-def parse_file(file_path):
-    if file_path.endswith('.json'):
-        file = json.load(open(file_path))
+def get_path(path):
+    path = os.path.expanduser(path)
+    path = os.path.abspath(os.path.normpath(path))
+    return path
+
+
+def get_data(file_path):
+    path = get_path(file_path)
+    _, ext = os.path.splitext(path)
+    if ext in {'.json'}:
+        data = json.load(open(path))
+    elif ext in {'.yml', '.yaml'}:
+        data = yaml.safe_load(open(path))
     else:
-        file = yaml.safe_load(open(file_path))
-    return file
+        data = dict()
+    return data
